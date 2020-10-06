9ca7479daeb4a9c084c7c82ae45dd1e274f3c4e0
Mackiewiczt867585 <58771542+Mackiewiczt867585@users.noreply.github.com>
2020-10-06 23:37:42 +0200

    Add files via upload

---
+++ LAB1/src/pl/uwm/edu/kek/Start.java
@@ -0,0 +1,14 @@
+package pl.uwm.edu.kek;
+
+public class Start
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  /////");
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("(| o o |)");
+        System.out.println(" |  ^  |   ");
+        System.out.println(" | `-' |");
+        System.out.println(" +-----+ ");
+    }
+}
